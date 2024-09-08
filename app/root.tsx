9a961a31ffa93f54a105845d9e7ad13ef12de6e6
Zwel <zwelhtetyandev@gmail.com>
2024-09-08 23:16:07 +0630

    init

---
+++ app/root.tsx
@@ -0,0 +1,144 @@
+console.log("hi");
+
+import {
+  Form,
+  Links,
+  Meta,
+  NavLink,
+  Outlet,
+  redirect,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+import {
+  ActionFunction,
+  LinksFunction,
+  LoaderFunctionArgs,
+  json,
+} from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+import { createEmptyContact, getContacts } from "./data";
+import { useEffect } from "react";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+export const action: ActionFunction = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <div>
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(e) => {
+                const isFirstSearch = q === null;
+                submit(e.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
+              <input
+                id="q"
+                aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                placeholder="Search"
+                type="search"
+                name="q"
+                defaultValue={q || ""}
+              />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
+            </Form>
+
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+
+          <h1>Remix Contacts</h1>
+        </div>
+
+        <div
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+          id="detail"
+        >
+          <Outlet />
+        </div>
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -0,0 +1,10 @@
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { deleteContact } from "~/data";
+
+export const action = async ({ params }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+
+  await deleteContact(params.contactId);
+  return redirect("/");
+};
